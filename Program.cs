74bcda41a620a847e14111cab4111e9c72e60f61
Trey Walker <Treywalk33@gmail.com>
2024-10-09 19:55:52 -0600

    step 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
 ﻿// Trey Walker, 10/9/2024, Madlibs
+
+// step two add discripion and greeting
+Console.WriteLine("This is Mad Libs. When you are dong providing the requested words");
+Console.WriteLine("I will build them into a funny story for you");
+Console.WriteLine("Enter the requested word types");
+
